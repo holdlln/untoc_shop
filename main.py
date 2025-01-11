37934b88c2f5cced76fc51dfc96628f319db4601
holdlln <profhb@pusan.ac.kr>
2025-01-12 07:41:52 +0900

    plzzzzz

--- main.py
+++ main.py
@@ -74,4 +74,4 @@ app.include_router(monetaryluck_router, tags=["monetaryluck"])
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
